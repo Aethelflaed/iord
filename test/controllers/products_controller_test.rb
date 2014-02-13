507a4d5f7cfcfcca7fa3585529036299b62cb7c4
Geoffroy PLANQUART <geoffroy@W530.aethelflaed.com>
2014-02-13 22:48:13 +0100

    added test for products_controller

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionController::TestCase
+  setup do
+    @product = create(:product)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get show" do
+    get :show, id: @product
+    assert_response :success
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @product
+    assert_response :success
+  end
+
+  test "should create" do
+    post :create, product: {name: 'Hello'}
+    assert_response 302
+  end
+
+  test "should update" do
+    patch :update, id: @product, product: {name: 'Hello'}
+    assert_redirected_to product_path(@product)
+  end
+
+  test "should destroy" do
+    delete :destroy, id: @product
+    assert_redirected_to products_path
+  end
+end
+
