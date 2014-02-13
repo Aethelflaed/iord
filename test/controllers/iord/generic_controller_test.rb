dcfcdf002a14f4aa8f3bc02d328d55f62c9cde94
Geoffroy PLANQUART <geoffroy@W530.aethelflaed.com>
2014-02-13 23:24:02 +0100

    tested generic:client

---
+++ test/controllers/iord/generic_controller_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+module Iord
+  class GenericControllerTest < ActionController::TestCase
+    setup do
+      @client = create(:client)
+    end
+
+    test "should get index" do
+      @request.path = "/clients"
+      get :index
+      assert_response :success
+    end
+
+    test "should get show" do
+      @request.path = "/clients/#{@client.id}"
+      get :show, id: @client
+      assert_response :success
+    end
+
+    test "should get new" do
+      @request.path = "/clients/new"
+      get :new
+      assert_response :success
+    end
+
+    test "should get edit" do
+      @request.path = "/clients/#{@client.id}/edit"
+      get :edit, id: @client
+      assert_response :success
+    end
+
+    test "should create" do
+      @request.path = "/clients"
+      post :create, client: {firstname: 'Hello'}
+      assert_response 302
+    end
+
+    test "should update" do
+      @request.path = "/clients/#{@client.id}"
+      patch :update, id: @client, client: {firstname: 'Hello'}
+      assert_redirected_to client_path(@client)
+    end
+
+    test "should destroy" do
+      @request.path = "/clients/#{@client.id}"
+      delete :destroy, id: @client
+      assert_redirected_to clients_path
+    end
+  end
+
+end
+
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -12,6 +12,7 @@ require "iord"
 
 module Dummy
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
