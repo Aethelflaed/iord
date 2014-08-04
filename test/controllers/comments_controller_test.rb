d7f41fbaf6f302c9f1d9093f3dfa5ad4429ec6b6
Geoffroy PLANQUART <geoffroy@W530.aethelflaed.com>
2014-08-04 09:50:40 +0200

    added hook tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'iord/hooks'
 
 class CommentsControllerTest < ActionController::TestCase
   setup do
@@ -37,23 +38,47 @@ class CommentsControllerTest < ActionController::TestCase
   end
 
   test "should create" do
+    hook_called = false
+    Iord::Hooks.register_hook(:comment, :create) do |resource, action|
+      assert action == :create
+      assert resource.persisted?
+      hook_called = true
+    end
+
     assert_difference("Product.find('#{@product_id}').comments.count") do
       post :create, product_id: @product_id, comment: {author_name: 'Hello'}
     end
     assert_response 302
+    assert hook_called
   end
-
+  
   test "should update" do
+    hook_called = false
+    Iord::Hooks.register_hook(:comment, :update) do |resource, action|
+      assert action == :update
+      assert resource.persisted?
+      hook_called = true
+    end
+
     request.path = "/products/#{@product_id}/comments/#{@comment.id}"
     patch :update, id: @comment.id, product_id: @product_id, comment: {author_name: 'World'}
     assert_redirected_to product_comment_path(@comment.commentable, @comment)
+    assert hook_called
   end
 
   test "should destroy" do
+    hook_called = false
+    Iord::Hooks.register_hook(:comment, :destroy) do |resource, action|
+      assert action == :destroy
+      assert resource.persisted? == false
+      hook_called = true
+    end
+
     assert_difference("Product.find('#{@product_id}').comments.count", -1) do
       delete :destroy, product_id: @product_id, id: @comment
     end
     assert_redirected_to product_comments_path
+    assert hook_called
   end
 end
 
